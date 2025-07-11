3947ee1e8078055693fb3b48bcd45ba6f89b3ab3
BeNikk <bhattnik442@gmail.com>
2025-07-11 16:22:29 +0530

    feat:add commit messages when editing a project

--- src/controllers/editProject.ts
+++ src/controllers/editProject.ts
@@ -1,14 +1,17 @@
-import { Request, Response } from 'express';
-import { updateProjectDataFile } from '../services/updatRepo';
+import { Request, Response } from "express";
+import { updateProjectDataFile } from "../services/updateRepo";
 
 export const handleEditProject = async (req: Request, res: Response) => {
-    const { repoName, projectData } = req.body;
+  const { repoName, projectData, commitMessage } = req.body;
 
-    try {
-        await updateProjectDataFile(repoName, projectData);
-        res.status(200).json({ message: 'Project updated successfully' });
-    } catch (error) {
-        console.error('Error updating project:', error);
-        res.status(500).json({ error: 'Failed to update project' });
+  try {
+    if (!commitMessage) {
+      res.status(300).json({ message: "Commit message is required" });
     }
+    await updateProjectDataFile(repoName, projectData, commitMessage);
+    res.status(200).json({ message: "Project updated successfully" });
+  } catch (error) {
+    console.error("Error updating project:", error);
+    res.status(500).json({ error: "Failed to update project" });
+  }
 };
